f9e6cc2312eecb092e299fdaf943147fd14ab8ae
Deshpande Shaunak Sudhir <ShaunakSudhir.Deshpande@infineon.com>
2025-05-26 01:20:55 +0530

    Rudimentary implementation of picking overlap logic added

--- main.c
+++ main.c
@@ -11,11 +11,11 @@
 #define SCREEN_WIDTH        800
 #define SCREEN_HEIGHT       400
 
-
 #define ACCELERATE_FACTOR   180
 #define MAX_ACCELERATION_FACTOR 10.0f
 #define MAX_ANGULAR_VELOCITY    15.0f
 #define ACCELERATION_DAMPENER   0.3f 
+#define DEFAULT_BREAK_ANGLE_SIZE    30.0f
 
 
 int main()
@@ -38,6 +38,13 @@ int main()
     const float PICK_RADIUS_SCALE = 1.2f;
     const float PICK_WIDTH_DEGREES = 10.0f;
     const int PICK_SECTOR_SEGMENTS = 30;
+
+    /* Hardcoded for now. Base should be a randomised value between 0 - 360.
+     * size should be set via a difficulty meter ideally.
+     * that can be another TODO. */
+    float BreakAngleBase = 270;
+    float BreakAngleSize = DEFAULT_BREAK_ANGLE_SIZE;
+    float PickDelta = PICK_WIDTH_DEGREES;
 
     while(!WindowShouldClose())
     {
@@ -76,13 +83,25 @@ int main()
         if(CurrentPickAngle < 0.0f)
             CurrentPickAngle += 360.0f;
 
+        /* Full overlap. Handle partial overlap later*/
+        if(CurrentPickAngle > BreakAngleBase &&
+            CurrentPickAngle + PickDelta <= BreakAngleBase + BreakAngleSize)
+        {
+            /* Wiggle the pick, play a sound, whatever. For now, let's make the 
+             * pick fatter till it takes up the entire sector, or falls out. */
+            PickDelta+= 0.1f;
+        }
         BeginDrawing();
             ClearBackground(GetColor(MAIN_WINDOW_COLOR));
             DrawCircle(PickLockPosX,PickLockPosY,LockRadius,GetColor(LOCK_COLOR));
             DrawCircleSector((Vector2){PickLockPosX,PickLockPosY}, 
                 (PICK_RADIUS_SCALE)*LockRadius, 
-                CurrentPickAngle, CurrentPickAngle + PICK_WIDTH_DEGREES, 
+                CurrentPickAngle, CurrentPickAngle + PickDelta, 
                 PICK_SECTOR_SEGMENTS, GetColor(PICK_COLOR));
+            /* There's no need to draw this, keeping it as a visual aid for now */
+            DrawCircleSectorLines((Vector2){PickLockPosX,PickLockPosY},
+            LockRadius, BreakAngleBase, BreakAngleBase + BreakAngleSize,
+            PICK_SECTOR_SEGMENTS, GREEN);
         EndDrawing();
     }
 
